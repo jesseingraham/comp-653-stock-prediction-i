58b4fe28a470eb1b68d4914d7c198ddc39eb41b9
Jesse Ingraham <118003082+jesseingraham@users.noreply.github.com>
2026-07-06 21:14:43 -0500

    Update config.py
    Including common configs such as path to shared Drive and the yfinance ticker/dates.

--- config.py
+++ config.py
@@ -1,1 +1,22 @@
+import os
 
+# ==========================================
+# FILE PATHS (DEFAULT)
+# ==========================================
+# Direct path to the Google Shared Drive
+DRIVE_ROOT = "/content/drive/Shareddrives/COMP 653 - Final Project"
+
+# Sub-directories for organization
+DRIVE_DATA_PATH = os.path.join(DRIVE_ROOT, "raw_data")
+DRIVE_MODELS_PATH = os.path.join(DRIVE_ROOT, "saved_models")
+
+# ==========================================
+# GLOBAL PROJECT SETTINGS
+# ==========================================
+TICKER = "^GSPC"              # S&P 500 symbol in yfinance
+START_DATE = "2006-01-01"     # ~20 years of data
+END_DATE = "2026-01-01"
+
+# Safety check for debugging
+if not os.path.exists(DRIVE_ROOT):
+    print(f"⚠️ WARNING: Could not find {DRIVE_ROOT}. Did you run drive.mount('/content/drive')?")
